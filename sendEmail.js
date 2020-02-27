16dd285f243ab407d2fdcd1783425f4ea3c52231
andrew sotskyi <andrewsotskyi@gmail.com>
2020-02-26 19:05:39 -0600

    change logo

--- sendEmail.js
+++ sendEmail.js
@@ -1,48 +1,37 @@
 window.addEventListener("DOMContentLoaded", function() {
+  let form = document.getElementById("my-form");
+  let button = document.getElementById("my-form-button");
+  let status = document.getElementById("my-form-status");
 
-    // get the form elements defined in your form HTML above
-    
-    let form = document.getElementById("my-form");
-    let button = document.getElementById("my-form-button");
-    let status = document.getElementById("my-form-status");
-    
-
-
-    // Success and Error functions for after the form is submitted
-    
-    function success() {
-      form.reset();
-      button.style = "display: none ";
-      status.innerHTML = "Thanks!";
-    }
-
-    function error() {
-      status.innerHTML = "Oops! There was a problem.";
-    }
-
-    // handle the form submission event
-
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      let data = new FormData(form);
-      console.log(data.forEach(elem=>console.log(elem)));
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
-  
-  // helper function for sending an AJAX request
-
-  function ajax(method, url, data, success, error) {
-    let xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
+  function success() {
+    form.reset();
+    button.style = "display: none ";
+    status.innerHTML = "Thanks!";
   }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    let data = new FormData(form);
+    console.log(data.forEach(elem => console.log(elem)));
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+function ajax(method, url, data, success, error) {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
